30d75c5f6049d7f06ef7d0414e3474f41214ba4b
Rajarshi Das <rd@rajarshi.me>
2025-12-31 00:12:12 +0530

    Arkafe.com offers og:icons - 8

--- SampleApp/App/Controller/W3/Portal/offers.php
+++ SampleApp/App/Controller/W3/Portal/offers.php
@@ -6,10 +6,10 @@ $data['og_type']                = 'website';
 $data['og_title']               = 'Happy New Year';
 $data['og_site_name']           = 'Happy New Year';
 $data['og_description']         = 'Grab exciting New Year offers';
-$data['og_image_secure_url']    = BASE_URL.'images/arkafe-offers.png';
+$data['og_image_secure_url']    = BASE_URL.'images/arkafe-2026.png';
 $data['og_url']                 = BASE_URL;
 $data['twitter_card']           = 'Arkafe';
 $data['twitter_image_alt']      = 'Arkafe';
-$data['apple_touch_icon']       = BASE_URL.'images/arkafe-offers.png';
+$data['apple_touch_icon']       = BASE_URL.'images/arkafe-2026.png';
 
 view($route, "offers2026", $data);
